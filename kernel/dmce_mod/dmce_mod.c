597775edb8eaa91d9f59078302eef1f0a4a0e3cd
IIAndreasII <andreas.bartilson@gmail.com>
2024-04-03 16:10:20 +0200

    Dynamically decide allocation method

--- kernel/dmce_mod/dmce_mod.c
+++ kernel/dmce_mod/dmce_mod.c
@@ -7,29 +7,34 @@
 #include <linux/init.h>
 #include <linux/atomic.h>
 #include <linux/printk.h>
-
+#include <asm/page.h>
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
 #define HAVE_PROC_OPS
 #endif
 
-#define PROCFS_MAX_SIZE 1024
+#define PROCFS_MAX_SIZE (1024 * 8)
 
 #define PROCFS_NAME "dmce_hmk"
+
+
+#define DMCE_ALLOC(size) (size < PAGE_SIZE ? kmalloc(size, GFP_KERNEL) : vmalloc(size))
+
+#define DMCE_FREE(size, ptr) do { \
+    if (size < PAGE_SIZE) \
+        kfree(ptr); \
+    else \
+        vfree(ptr); \
+} while(0);
 
 static struct proc_dir_entry *proc_file;
 
 static size_t procfs_buffer_size = 0;
 
-extern atomic64_t* dmce_magic;
-//extern atomic_t dmce_buffer[];
+extern atomic64_t* dmce_buffer;
 extern int nbr_probes;
 
-atomic_t __attribute__((common)) alloced = ATOMIC_INIT(0);
-
-int magic = 0xAAAD11CE;
-
-int* magic_buf;
+atomic_t __attribute__((common)) dmce_buffer_allocated = ATOMIC_INIT(0);
 
 static ssize_t proc_read(struct file *fp, char __user *buf, size_t buf_len, loff_t *offset)
 {
@@ -40,21 +45,21 @@ static ssize_t proc_read(struct file *fp, char __user *buf, size_t buf_len, loff
         return 0;
     }
 
-    int* dmce_tmp_buffer = kmalloc(sizeof(int) * nbr_probes, GFP_KERNEL);
+    int* dmce_tmp_buffer = DMCE_ALLOC(sizeof(long long) * nbr_probes);
 
     for (size_t i = 0; i < nbr_probes; i++)
     {
-        dmce_tmp_buffer[i] = atomic_fetch_add(0, &dmce_magic[i]);
+        dmce_tmp_buffer[i] = atomic64_fetch_add(0, &dmce_buffer[i]);
     }
 
     procfs_buffer_size = min(sizeof(int) * nbr_probes, buf_len);
 
     if (copy_to_user(buf + *offset, dmce_tmp_buffer + *offset, procfs_buffer_size))
     {
-        kfree(dmce_tmp_buffer);
+        DMCE_FREE(sizeof(long long) * nbr_probes, dmce_tmp_buffer);
         return -EFAULT;
     }
-    kfree(dmce_tmp_buffer);
+    DMCE_FREE(sizeof(long long) * nbr_probes, dmce_tmp_buffer);
 
 
     *offset += procfs_buffer_size;
@@ -69,7 +74,7 @@ static ssize_t proc_write(struct file *fp, const char __user *buf, size_t buf_le
 {
     procfs_buffer_size = min(PROCFS_MAX_SIZE, buf_len);
 
-    if (copy_from_user(dmce_magic, buf, procfs_buffer_size))
+    if (copy_from_user(dmce_buffer, buf, procfs_buffer_size))
         return -EFAULT;
 
     *offset += procfs_buffer_size;
@@ -94,9 +99,6 @@ static const struct file_operations proc_file_fops = {
 
 int __init init_module(void)
 {
-    magic_buf = kmalloc(sizeof(int) * 1, GFP_KERNEL);
-    magic_buf[0] = magic;
-    //printk("dmce_mod: magic buffer allocated at: %px\n", magic_buf);
     proc_file = proc_create(PROCFS_NAME, 0644, NULL, &proc_file_fops);
     if (NULL == proc_file)
     {
@@ -112,7 +114,6 @@ int __init init_module(void)
 
 void __exit cleanup_module(void)
 {
-    kfree(magic_buf);
     proc_remove(proc_file);
     pr_info("/proc/%s removed\n", PROCFS_NAME);
 }
